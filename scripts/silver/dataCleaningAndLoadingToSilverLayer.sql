636cc97bcc927ddd67ba4fbfe5d301bf4381599b
Sameer Mohammad <sameertas12@gmail.com>
2025-10-25 23:30:10 +0530

    Update dataCleaningAndLoadingToSilverLayer.sql

--- scripts/silver/dataCleaningAndLoadingToSilverLayer.sql
+++ scripts/silver/dataCleaningAndLoadingToSilverLayer.sql
@@ -237,3 +237,278 @@ SELECT
 FROM bronze.crm_prod_info;
 
 -- -----------------------------------------------------------------------------------
+
+-- crm_sales_details Table
+SELECT * FROM bronze.crm_sales_details;
+
+-- Checking for any Extra spaces in sls_ord_num column
+SELECT sls_ord_num
+FROM bronze.crm_sales_details
+WHERE sls_ord_num != TRIM(sls_ord_num);
+
+-- From Architecture we can observe that sls_prd_key is linked with prd_key of product info table
+-- So lets check if there are any sls_prd_key in crm_sales_details table that are not in product info table
+SELECT
+	sls_ord_num,
+    sls_prd_key,
+    sls_quantity
+FROM bronze.crm_sales_details
+WHERE sls_prd_key NOT IN(
+	SELECT prd_key FROM silver.crm_prod_info
+);
+-- O/P: No such keys found
+
+-- From Architecture we can observe that sls_cust_id is linked with cst_id of customer info table
+-- So lets check if there are any ls_cust_id in crm_sales_details table that are not in customer info table
+SELECT
+	sls_ord_num,
+    sls_cust_id,
+    sls_quantity
+FROM bronze.crm_sales_details
+WHERE sls_cust_id NOT IN(
+	SELECT cst_id FROM silver.crm_cust_info
+);
+-- O/P: No such keys found
+
+-- sls_order_dt, sls_ship_dt, sls_due_dt are in Number Format
+-- Lets convert it into Appropriate Date Fromat
+SELECT
+	*,
+    CASE
+		WHEN sls_order_dt<=0 OR LENGTH(sls_order_dt)!=8 THEN NULL
+        ELSE STR_TO_DATE(CAST(sls_order_dt AS CHAR), '%Y%m%d')
+    END AS sls_order_dt,
+    CASE
+		WHEN sls_ship_dt<=0 OR LENGTH(sls_ship_dt)!=8 THEN NULL
+        ELSE STR_TO_DATE(CAST(sls_ship_dt AS CHAR), '%Y%m%d')
+    END AS sls_ship_dt,
+    CASE
+		WHEN sls_due_dt<=0 OR LENGTH(sls_due_dt)!=8 THEN NULL
+        ELSE STR_TO_DATE(CAST(sls_due_dt AS CHAR), '%Y%m%d')
+    END AS sls_due_dt
+FROM bronze.crm_sales_details;
+
+-- Checking if there are any NULL and Negative values in sls_sales, sls_quantity, sls_price columns
+-- Also check if there are any sales where price*quantity != sales
+SELECT * FROM bronze.crm_sales_details
+WHERE sls_sales <=0 OR sls_sales IS NULL
+	OR sls_quantity <=0 OR sls_quantity IS NULL
+	OR sls_price <=0 OR sls_price IS NULL
+    OR sls_sales != sls_price*sls_quantity;
+
+-- Now Correct all the errors
+SELECT
+	*,
+    CASE
+		WHEN sls_sales <=0 OR sls_sales IS NULL OR sls_sales != ABS(sls_price)*sls_quantity 
+        THEN ABS(sls_price)*sls_quantity
+        ELSE sls_price
+    END AS sls_sales,
+    sls_quantity,
+    CASE
+		WHEN sls_price <=0 OR sls_price IS NULL OR sls_price != sls_sales/ABS(sls_quantity)
+        THEN sls_sales/NULLIF(sls_quantity, 0)
+        ELSE sls_price
+    END AS sls_price
+FROM bronze.crm_sales_details;
+
+-- Merging all queries
+SELECT
+	sls_ord_num,
+    sls_prd_key,
+    sls_cust_id,
+    CASE
+		WHEN sls_order_dt<=0 OR LENGTH(sls_order_dt)!=8 THEN NULL
+        ELSE STR_TO_DATE(CAST(sls_order_dt AS CHAR), '%Y%m%d')
+    END AS sls_order_dt,
+    CASE
+		WHEN sls_ship_dt<=0 OR LENGTH(sls_ship_dt)!=8 THEN NULL
+        ELSE STR_TO_DATE(CAST(sls_ship_dt AS CHAR), '%Y%m%d')
+    END AS sls_ship_dt,
+    CASE
+		WHEN sls_due_dt<=0 OR LENGTH(sls_due_dt)!=8 THEN NULL
+        ELSE STR_TO_DATE(CAST(sls_due_dt AS CHAR), '%Y%m%d')
+    END AS sls_due_dt,
+    CASE
+		WHEN sls_sales <=0 OR sls_sales IS NULL OR sls_sales != ABS(sls_price)*sls_quantity 
+        THEN ABS(sls_price)*sls_quantity
+        ELSE sls_price
+    END AS sls_sales,
+    sls_quantity,
+    CASE
+		WHEN sls_price <=0 OR sls_price IS NULL OR sls_price != sls_sales/ABS(sls_quantity)
+        THEN sls_sales/NULLIF(sls_quantity, 0)
+        ELSE sls_price
+    END AS sls_price
+FROM bronze.crm_sales_details;
+
+-- Before Inserting sls_order_dt, sls_ship_dt, sls_due_dt are INT type in silver layer
+-- We should first change the datatype to DATE 
+ALTER TABLE silver.crm_sales_details
+MODIFY COLUMN sls_order_dt DATE,
+MODIFY COLUMN sls_ship_dt DATE,
+MODIFY COLUMN sls_due_dt DATE;
+
+-- Now Insert this cleaned data into silver layer
+INSERT INTO silver.crm_sales_details(
+	sls_ord_num,
+    sls_prd_key,
+    sls_cust_id,
+    sls_order_dt,
+    sls_ship_dt,
+    sls_due_dt,
+    sls_sales,
+    sls_quantity,
+    sls_price
+)
+SELECT
+	sls_ord_num,
+    sls_prd_key,
+    sls_cust_id,
+    CASE
+		WHEN sls_order_dt<=0 OR LENGTH(sls_order_dt)!=8 THEN NULL
+        ELSE STR_TO_DATE(CAST(sls_order_dt AS CHAR), '%Y%m%d')
+    END AS sls_order_dt,
+    CASE
+		WHEN sls_ship_dt<=0 OR LENGTH(sls_ship_dt)!=8 THEN NULL
+        ELSE STR_TO_DATE(CAST(sls_ship_dt AS CHAR), '%Y%m%d')
+    END AS sls_ship_dt,
+    CASE
+		WHEN sls_due_dt<=0 OR LENGTH(sls_due_dt)!=8 THEN NULL
+        ELSE STR_TO_DATE(CAST(sls_due_dt AS CHAR), '%Y%m%d')
+    END AS sls_due_dt,
+    CASE
+		WHEN sls_sales <=0 OR sls_sales IS NULL OR sls_sales != ABS(sls_price)*sls_quantity 
+        THEN ABS(sls_price)*sls_quantity
+        ELSE sls_price
+    END AS sls_sales,
+    sls_quantity,
+    CASE
+		WHEN sls_price <=0 OR sls_price IS NULL OR sls_price != sls_sales/ABS(sls_quantity)
+        THEN sls_sales/NULLIF(sls_quantity, 0)
+        ELSE sls_price
+    END AS sls_price
+FROM bronze.crm_sales_details;
+
+-- -----------------------------------------------------------------------------------
+
+-- erp_cust_az12 Table
+SELECT * FROM bronze.erp_cust_az12;
+
+-- cid from erp_cust_az12 Table and cst_key from crm_cust_info are common columns 
+-- but in cid we have NAS extra a prefix. we should fix this.
+SELECT
+	*,
+    CASE
+		WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LENGTH(cid))
+        ELSE cid
+    END as cid
+FROM bronze.erp_cust_az12;
+
+-- bdate column has dates which are from FUTURE i.e, after Today
+-- and there are customers whose age is more than 100 years, i.e, bdate before 1925-01-01
+SELECT bdate
+FROM bronze.erp_cust_az12
+WHERE bdate > CURDATE() OR bdate<'1924-01-01';
+
+SELECT
+	CASE
+		WHEN bdate > CURDATE() THEN NULL
+        ELSE bdate
+    END AS bdate
+FROM bronze.erp_cust_az12;
+
+-- Gender column is inappropriate. 
+SELECT 
+	DISTINCT gen,
+    COUNT(*) AS Cnt
+FROM bronze.erp_cust_az12
+GROUP BY gen;
+
+-- Lets fix it
+-- SET SQL_SAFE_UPDATES = 0;
+-- UPDATE bronze.erp_cust_az12
+-- SET gen = CASE
+--   WHEN REGEXP_REPLACE(UPPER(gen), '[^A-Z]', '') IN ('MALE','M') THEN 'MALE'
+--   WHEN REGEXP_REPLACE(UPPER(gen), '[^A-Z]', '') IN ('FEMALE','F') THEN 'FEMALE'
+--   ELSE 'N/A'
+-- END;
+-- SET SQL_SAFE_UPDATES = 1;
+
+-- Now Merge the Queries
+SELECT
+	CASE
+		WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LENGTH(cid))
+        ELSE cid
+    END as cid,
+    CASE
+		WHEN bdate > CURDATE() THEN NULL
+        ELSE bdate
+    END AS bdate,
+    gen
+FROM bronze.erp_cust_az12;
+
+-- Now Insert the data into silver layer
+INSERT INTO silver.erp_cust_az12()
+SELECT
+	CASE
+		WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LENGTH(cid))
+        ELSE cid
+    END as cid,
+    CASE
+		WHEN bdate > CURDATE() THEN NULL
+        ELSE bdate
+    END AS bdate,
+    gen
+FROM bronze.erp_cust_az12;
+
+-- -----------------------------------------------------------------------------------
+
+-- erp_loc_a101 Table
+SELECT * FROM bronze.erp_loc_a101;
+
+-- In cid column '-' should be removed
+SELECT
+	cid,
+    REPLACE(cid, '-', '') as cid
+FROM bronze.erp_loc_a101;
+
+-- Fixing Country column
+-- SET SQL_SAFE_UPDATES = 0;
+-- UPDATE bronze.erp_loc_a101
+-- SET cntry = CASE
+--   WHEN REGEXP_REPLACE(TRIM(cntry), '[^A-Z]', '') = 'DE' THEN 'Germany'
+--   WHEN REGEXP_REPLACE(TRIM(cntry), '[^A-Z]', '') IN('US', 'USA') THEN 'United States'
+--   WHEN REGEXP_REPLACE(TRIM(cntry), '[^A-Z]', '') = '' OR cntry IS NULL THEN 'N/A'
+--   ELSE TRIM(cntry)
+-- END;
+-- SET SQL_SAFE_UPDATES = 1;
+
+SELECT
+	cntry1,
+    COUNT(*) AS Cnt
+FROM(
+SELECT
+	cntry,
+    CASE
+		WHEN cntry LIKE '%Germany%' THEN 'Germany'
+        WHEN cntry LIKE '%States%' THEN 'United States'
+        ELSE cntry
+    END AS cntry1
+FROM bronze.erp_loc_a101
+) AS t
+GROUP BY cntry1;
+
+-- Merging Queries and Inserting into Silver Layer
+INSERT INTO silver.erp_loc_a101()
+SELECT
+	REPLACE(cid, '-', '') as cid,
+    CASE
+		WHEN cntry LIKE '%Germany%' THEN 'Germany'
+        WHEN cntry LIKE '%States%' THEN 'United States'
+        ELSE cntry
+    END AS cntry1
+FROM bronze.erp_loc_a101;
+
+-- -----------------------------------------------------------------------------------
+
